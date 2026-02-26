44b24b9d00e2186e2c3677949fafb64ec5ee8068
Alex032911 <anavarrocasillas@gmail.com>
2026-02-26 10:44:19 +0100

    He corregido alguna cosa de las 5 primeras clases y he creado un fichero para que importemos ahí las clases y hagamos la ejecución del código

--- proyecto_final.py
+++ proyecto_final.py
@@ -9,6 +9,9 @@ class Cancion:
         self.genero=genero
         self.reproducciones_cancion=0
 
+    def __str__(self):
+        return f"{self.titulo}"
+
     def repros(self):
         self.reproducciones_cancion+=1
         Cancion.reproducciones+=1
@@ -33,7 +36,10 @@ class Album:
 
     def reproducir_album(self):
         self.reproducciones_album+=1
-        print(f"El álbum tiene {self.reproducciones_album} reproducciones")
+        if self.reproducciones_album==1:
+            print(f"El álbum {self.titulo_album} tiene 1 reproducción")
+        else:
+            print(f"El álbum {self.titulo_album} tiene {self.reproducciones_album} reproducciones")
 
 class Artista:
     total_artistas=0
@@ -41,25 +47,33 @@ class Artista:
         self.nombre=nombre
         self.pais=pais
         self.edad=edad
+        self.canciones=[]
         self.albumes=[]
-    total_artistas+=1
+        Artista.total_artistas+=1
+
+    def agregar_canciones(self,cancion):
+        self.canciones.append(cancion)
 
     def agregar_album(self,album):
         self.albumes.append(album)
-        cantidad_albumes_artista=len(self.albumes)
-        print(f"La discografía de {self.nombre} posee un total de {cantidad_albumes_artista} álbumes")
-        return cantidad_albumes_artista
 
     def total_canciones(self):
         total=0
         for album in self.albumes:
             total+=album.numero_canciones_album()
-        print(f"{self.nombre} tiene {total} canciones en total")
+        total+=len(self.canciones)
+        if total==1:
+            print(f"{self.nombre} tiene 1 canción en total")
+        else:
+            print(f"{self.nombre} tiene {total} canciones en total")
         return total
 
     def numero_albumes(self):
         cantidad_albumes=len(self.albumes)
-        print(f"El artista tiene {cantidad_albumes} álbum/es en total")
+        if cantidad_albumes==1:
+            print(f"{self.nombre} tiene 1 álbum en total")
+        else:
+            print(f"{self.nombre} tiene {cantidad_albumes} álbumes en total")
         return cantidad_albumes
 
 class Playlist:
@@ -96,7 +110,7 @@ class Usuario:
 
     def cantidad_playlists(self):
         numero_playlists=len(self.playlists_usuario)
-        print(f"{self.playlists_usuario} tiene {numero_playlists} playlists guardadas")
+        print(f"{self.nombre_usuario} tiene {numero_playlists} playlists guardadas")
         return numero_playlists
 class Genero:
     def __init__(self, nombre_genero, descripcion):
@@ -107,6 +121,7 @@ class Genero:
     def añadir_cancion_a_genero(self, cancion):
         self.canciones_este_genero.append(cancion)
         print(f"Canción '{cancion.titulo}' añadida a la categoría {self.nombre_genero}")
+
 class EstadoAnimo:
     def __init__(self, tipo_animo, color):
         self.tipo_animo = tipo_animo
@@ -119,14 +134,14 @@ class EstadoAnimo:
 class BibliotecaGlobal:
     total_canciones_biblioteca = 0 # Atributo de clase (como el de Artista de Alex)
 
-    def __init__(self, dueño):
-        self.dueño = dueño
+    def __init__(self, duenyo):
+        self.duenyo = duenyo
         self.todas_las_canciones = []
 
     def registrar_nueva_cancion(self, cancion):
         self.todas_las_canciones.append(cancion)
         BibliotecaGlobal.total_canciones_biblioteca += 1
-        print(f"Biblioteca de {self.dueño}: Ahora hay {BibliotecaGlobal.total_canciones_biblioteca} canciones en total")
+        print(f"Biblioteca de {self.duenyo}: Ahora hay {BibliotecaGlobal.total_canciones_biblioteca} canciones en total")
 
 
 
